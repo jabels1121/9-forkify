976fe23ccd7629f4dfb495acaee844fe3d2b56b9
Jabels <jabels1121@gmail.com>
2018-12-03 14:40:33 +0300

    Implemented controlSearch function in the app controller by created functions in view controller and model controller which fetching data from public api and display results in the UI.

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,44 @@
 // Global app controller
 import Search from './models/Search';
+import { elements} from "./views/base";
+import * as searchView from './views/searchView';
 
 const search = new Search('pizza');
 console.log(search);
-search.getResult();
 
+/*
+    - Global state of the app:
+        - Search object
+        - Current recipe object
+        - Shopping list object
+        - Liked recipes
+ */
+const state = {};
 
+const controlSearch = async () => {
+      // 1. Get query from the view
+    const query = searchView.getInput();
 
+    if (query) {
+        // 2. Create new Search object and add it to state;
+        state.search = new Search(query);
+
+        // 3. Prepare UI for the results
+        searchView.clearInput();
+        searchView.clearResults();
+
+        // 4. Search for recipes
+        await state.search.getResult();
+
+        // 5. Render results on UI.
+        searchView.renderResults(state.search.result);
+    }
+};
+
+elements.searchFrom.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
 
 
 
---
+++ src/js/views/base.js
@@ -0,0 +1,5 @@
+export const elements = {
+    searchInput: document.querySelector('.search__field'),
+    searchFrom: document.querySelector('.search'),
+    searchResList: document.querySelector('.results__list')
+};
